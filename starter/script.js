6eac2301d9a5b83b228867d98de3a967590f0a88
ghoshsameer35 <84507931+ghoshsameer35@users.noreply.github.com>
2022-02-13 09:40:30 +0530

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -67,7 +67,7 @@ btnHold.addEventListener('click', function(){
     
     document.getElementById(`score--${activePlayer}`).textContent=scores[activePlayer];
 
-if (scores[activePlayer]>=20){
+if (scores[activePlayer]>=100){
     diceEl.classList.add('hidden')
 playing = false
  document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
